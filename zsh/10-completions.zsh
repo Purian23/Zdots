d4b440b8ffbfcbd19f55a83b1c4a69d76e0aa610
bbedward <bbedward@gmail.com>
2025-08-31 22:23:07 -0400

    fzf-tab update

--- zsh/10-completions.zsh
+++ zsh/10-completions.zsh
@@ -10,6 +10,10 @@ zstyle ':completion:*' menu select
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh/cache
 
-# ---- fzf-tab Styles ----
+# ---- fzf-tab styles ----
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
 zstyle ':fzf-tab:*' switch-group '<' '>'
+
+
